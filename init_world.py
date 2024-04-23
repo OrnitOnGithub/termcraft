5f3a4d0275000e4c6bfcbc00e65da2eec2e1bb98
ornit <ornithopterthethopter@gmail.com>
2024-04-23 22:37:26 +0200

    todo: draw world

---
+++ init_world.py
@@ -0,0 +1,8 @@
+# Create a test world of 100% grass
+world_size = 255*255*255
+print("creating "+ str(world_size) + " sized world")
+with open("world.rmc", "w") as file:
+  file.write("")
+with open("world.rmc", "a") as file:
+  for _ in range(world_size):
+    file.write("1")
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 #![allow(dead_code)] // temporary
-
-
 use colored::*;
+
+// unintuitively, Y is horizontal
+const SCREEN_Y_SIZE: usize = 50;
+// and X is vertical
+const SCREEN_X_SIZE: usize = 30;
+
+const PI: f32 = 3.14159265358979323846264338327950288419716939937510582;
+
 const CAMERA_DISTANCE: f32 = 10.0;
 const SUN_DIRECTION: Vector3 = Vector3{x: 0.0, y: 0.0, z: -1.0};
 
-const PIXEL_CHAR: String = String::from("MM");
-
-const PI: f32 = 3.14159265358979323846264338327950288419716939937510582;
 
 #[cfg(test)]
 mod tests {
@@ -38,7 +41,7 @@ mod tests {
     fn projection_2d_to_1d() {
       // test data
       let camera_position: Vector2 = Vector2 { x: 1.0, y: 1.0 };
-      let camera_rot: f32 = 45.0;
+      let camera_rot: f32 = 45.0 * (PI/180.0);
       let vertex: Vector2 = Vector2 { x: 12.0, y: 12.0 };
 
       let actual: f32 = project_2d_to_1d(vertex, camera_position, camera_rot);
@@ -52,25 +55,92 @@ mod tests {
       // the camera should be pointing directly at the vertex
       let vertex: Vector3 = Vector3 { x: 10.0, y: 10.0, z: 1.0 };
       let camera_position: Vector3 = Vector3 { x: 1.0, y: 1.0, z: 1.0 };
-      let camera_rotation_horizontal: f32 = 45.0;
-      let camera_rotation_vertical: f32 = 0.0;
+      let camera_rotation_horizontal: f32 = 45.0 * (PI/180.0);
+      let camera_rotation_vertical: f32 = 0.0 * (PI/180.0);
       // so expected result is (0, 0)
       let actual: Vector2 = render_vertex(vertex, camera_position, camera_rotation_vertical, camera_rotation_horizontal);
       let expected: Vector2 = Vector2 { x: 0.0, y: 0.0 };
 
       assert_eq!(actual, expected);
     }
+
+    #[test]
+    fn triangle_contains() {
+      let a: Vector2 = Vector2 { x: 0.0, y: 0.0 };
+      let c: Vector2 = Vector2 { x: 0.0, y: 12.0 };
+      let b: Vector2 = Vector2 { x: 12.0, y: 0.0 };
+      let triangle: Triangle2D = Triangle2D { a, b, c, color: CustomColor { r: 0, g: 0, b: 0 } };
+
+      let expected_1: bool = true;
+      let actual_1: bool = triangle.contains(Vector2 { x: 2.0, y: 2.0 });
+      assert_eq!(actual_1, expected_1);
+
+      let expected_2: bool = false;
+      let actual_2: bool = triangle.contains(Vector2 { x: 12.0, y: 12.0 });
+      assert_eq!(actual_2, expected_2);
+    }
+
 }
 
 fn main() {
   let mut camera_position: Vector3 = Vector3 { x: 0.0, y: 0.0, z: 0.0 };
-  let mut camera_rotation_vertical: f32 = 0.0;
-  let mut camera_rotation_horizontal: f32 = 0.0;
+  // achtung! CAMERA ROTATION IS IN RADIANS
+  let mut camera_rotation_vertical: f32 = 0.0 * (PI/180.0);
+  let mut camera_rotation_horizontal: f32 = 0.0 * (PI/180.0);
 
+  // initialise the screen
+  let mut main_screen: Screen = Screen { pixels: Vec::new(), size_x: SCREEN_X_SIZE, size_y: SCREEN_Y_SIZE };
+  main_screen.init();
+
+  // initialise the world
+  // put the whole world in memory because reading from disk is slow
+  let mut world: Vec<CubeType> = load_world("world.rmc");
+
+  // GAME LOOP
+  loop {
+    //clearscreen::clear().expect("failed to clear screen");
+    
+    // update main screen
+    main_screen = draw_world();
+    // update main screen
+
+    // Draw the screen and sleep for a few milliseconds (to let the screen render)
+    main_screen.draw();
+    let idle_interval = std::time::Duration::from_millis(10);
+    std::thread::sleep(idle_interval);
+  }
 }
 
-fn draw_screen(screen: Screen) -> () {
+fn draw_world() -> Screen {
 
+  todo!("TODO: draw_world");
+
+  // so the linter shuts up
+  return Screen { pixels: Vec::new(), size_x: 0, size_y: 0 };
+}
+
+fn load_world(path: &str) -> Vec<CubeType> {
+  let mut cubes: Vec<CubeType> = Vec::new();
+
+  let contents: String = std::fs::read_to_string(path)
+    .expect("Should have been able to read the file, or not idk");
+
+  for char in contents.as_bytes() {
+    match char {
+
+      b'0' => { cubes.push(CubeType::Air)   }
+      b'1' => { cubes.push(CubeType::Grass) }
+      b'2' => { cubes.push(CubeType::Stone) }
+      b'3' => { cubes.push(CubeType::Wood)  }
+
+      _ => {
+        panic!("invalid element in save file.");
+      }
+    }
+
+  }
+
+  return cubes
 }
 
 fn render_triangle(triangle: Triangle3D, camera_position: Vector3, camera_rotation_vertical: f32, camera_rotation_horizontal: f32) -> Triangle2D {
@@ -83,7 +153,7 @@ fn render_triangle(triangle: Triangle3D, camera_position: Vector3, camera_rotati
   return Triangle2D { a: vertex_a, b: vertex_b, c: vertex_c, color: test_colour }
 }
 
-/// 3D point -> 2D point (to be put on screen)
+/// 3D point -> 2D point (to be put on screen)██
 fn render_vertex(vertex: Vector3, camera_position: Vector3, camera_rotation_vertical: f32, camera_rotation_horizontal: f32) -> Vector2 {
 
   let position_on_vertical_plane: Vector2 = Vector2   { x: vertex.z, y: vertex.x };
@@ -114,12 +184,12 @@ fn project_2d_to_1d(vertex: Vector2, camera_position: Vector2, camera_rotation: 
 
   // distance between camera and the vertex
   let camera_vertex_distance: f32 = vector2_distance(camera_position.clone(), vertex.clone());
-  // TODO: calculate relative angly directly using relative position
+  // find the vector that connects camera and vertex
   let camera_vertex_distance_vector: Vector2 = Vector2 { x: vertex.x - camera_position.x, y: vertex.y - camera_position.y };
   // world angle of vertex relative to camera
   let vertex_relative_angle: f32 = f32::atan(camera_vertex_distance_vector.x/camera_vertex_distance_vector.y);
   // relative to camera angle of vertex relative to camera
-  let vertex_camera_angle: f32 = camera_rotation * (PI/180.0) - vertex_relative_angle;
+  let vertex_camera_angle: f32 = camera_rotation - vertex_relative_angle;
   
   // find depth
   let depth: f32 = f32::cos(vertex_camera_angle) * camera_vertex_distance;
@@ -189,6 +259,30 @@ struct Triangle2D {
   b: Vector2,
   c: Vector2,
   color: CustomColor,
+  //parent_cube_idex: usize,
+}
+impl Triangle2D {
+  /// Check if a triangle contains a certain point
+  fn contains(&self, point: Vector2) -> bool {
+    // say ABC = triangle and P = point:
+    // test if area ABC = area ABP + area ACP + area BCP
+    let real_triangle_area = triangle_area(self.clone());
+
+    let mut area_sum: f32 = 0.0;
+    area_sum += triangle_area(Triangle2D { a: self.a, b: self.b, c: point, color: CustomColor {r: 0, g: 0, b: 0} });
+    area_sum += triangle_area(Triangle2D { a: self.a, b: self.c, c: point, color: CustomColor {r: 0, g: 0, b: 0} });
+    area_sum += triangle_area(Triangle2D { a: self.b, b: self.c, c: point, color: CustomColor {r: 0, g: 0, b: 0} });
+
+    return real_triangle_area == area_sum;
+  }
+}
+/// calculate the area of a 2D triangle
+fn triangle_area(triangle: Triangle2D) -> f32 {
+  let ab: Vector2 = Vector2 { x: triangle.b.x - triangle.a.x, y: triangle.b.y - triangle.a.y };
+  let ac: Vector2 = Vector2 { x: triangle.c.x - triangle.a.x, y: triangle.c.y - triangle.a.y };
+  let cross_product: f32 = ab.x * ac.y - ab.y * ac.x;
+  let area = f32::abs(cross_product);
+  return area/2.0;
 }
 
 struct Screen {
@@ -197,7 +291,7 @@ struct Screen {
   size_y: usize,
 }
 impl Screen {
-  fn init(mut self) {
+  fn init(&mut self) {
     let mut screen: Vec<Vec<CustomColor>> = Vec::new();
     for _ in 0..self.size_x {
       let mut x_row: Vec<CustomColor> = Vec::new();
@@ -209,11 +303,22 @@ impl Screen {
     self.pixels = screen;
   }
   fn draw(&self) {
+    let pixel_char: ColoredString = ColoredString::from("██");
     for column in 0..self.size_x {
       for pixel in 0..self.size_y {
         let color: CustomColor = self.pixels[column][pixel];
-        print!("{}", PIXEL_CHAR);
+        print!("{}", pixel_char.clone().custom_color(color));
       }
+      println!();
     }
   }
+}
+
+/// enumarates all possible cubes
+#[derive(Debug, Clone, PartialEq)]
+enum CubeType {
+  Air,
+  Grass,
+  Stone,
+  Wood,
 }
